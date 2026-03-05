65ba4ddd4fd05e8a3be93bc19e45bf598222067e
Alex Huynh <174913689+ahuynh6265@users.noreply.github.com>
2026-03-04 21:04:53 -0500

    removed ssl

--- database.py
+++ database.py
@@ -7,11 +7,7 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"sslmode": "require"},
-    pool_pre_ping=True
-)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(
   autocommit=False,
