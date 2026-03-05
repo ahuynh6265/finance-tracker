e5818dfbb3721963ece3a5771996d0b80737fb93
Alex Huynh <174913689+ahuynh6265@users.noreply.github.com>
2026-03-04 20:40:34 -0500

    added connect args

--- database.py
+++ database.py
@@ -6,7 +6,9 @@ import os
 load_dotenv() 
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL)
+
+connect_args={}
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 
 SessionLocal = sessionmaker(
   autocommit=False,
