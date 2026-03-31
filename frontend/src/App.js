079e3260b5678f1620b90b560810566af6a1e861
Alex Huynh <174913689+ahuynh6265@users.noreply.github.com>
2026-03-30 20:53:59 -0400

    summary styling skeleton, and finished mapping frontend logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,17 +7,29 @@ import {BrowserRouter, Routes, Route, Link} from "react-router-dom"
 function App (){
   return(
   <BrowserRouter>
-    <Link to = "/">Summary</Link>
-    <Link to = "/categories">Categories</Link>
-    <Link to = "/accounts">Accounts</Link>
-    <Link to = "/transactions">Transactions</Link>
+    <div className = "navbar">
+      <div className = "text-white">Finance Tracker</div>
+      <hr className = "line"></hr>
+      <Link className = "nav-link" to = "/">Summary</Link>
+      <Link className = "nav-link" to = "/categories">Categories</Link>
+      <Link className = "nav-link" to = "/accounts">Accounts</Link>
+      <Link className = "nav-link" to = "/transactions">Transactions</Link>
 
-    <Routes>
-      <Route path = "/" element = {<Summary />}></Route>
-      <Route path = "/categories" element = {<Categories />}></Route>
-      <Route path = "/accounts" element = {<Accounts />}></Route>
-      <Route path = "/transactions" element = {<Transactions />}></Route>
-    </Routes>
+      <hr className = "mt-auto line"></hr>
+      <div className = "text-white">Support</div>
+      <div className = "text-white">Settings</div>
+      <div className = "text-white">Name</div>
+      
+    </div>
+
+    <div className = "main-content">
+      <Routes>
+        <Route path = "/" element = {<Summary />}></Route>
+        <Route path = "/categories" element = {<Categories />}></Route>
+        <Route path = "/accounts" element = {<Accounts />}></Route>
+        <Route path = "/transactions" element = {<Transactions />}></Route>
+      </Routes>
+    </div> 
   </BrowserRouter> )
 }
 
--- frontend/src/components/Summary.jsx
+++ frontend/src/components/Summary.jsx
@@ -11,11 +11,26 @@ function Summary() {
   }, [])
   if (!summary) return <div>Loading...</div> 
   else return (
-    <div className = "bg-slate-900 min-h-screen w-full flex flex-col gap-4 p-8">
-      <div className = "text-green-400 text-sm p-4 bg-slate-800 rounded-lg h-fit">Total Income: {summary.income}</div>
-      <div className = "text-red-400 text-sm p-4 bg-slate-800 rounded-lg h-fit">Total Expenses: {summary.expenses}</div>
-      <div className = "p-4 bg-slate-800 rounded-lg h-fit">Net Balance: {summary["net balance"]}</div>
-    </div> 
+    <div>
+      <div className = "page-title">Welcome, Alex</div>
+      <div className = "banner"></div>
+      <div className = "grid-layout">
+        <div className = "card row-span-3">My Accounts</div>
+        <div className = "col-span-2 stats-row">
+          <div className = "text-green-400 text-sm card">Total Income: 
+            <div className = "text-green-400 text-2xl">${summary.income}</div>
+          </div>
+          <div className = "text-red-400 text-sm card">Total Expenses: 
+          <div className = "text-red-400 text-2xl">${summary.expenses}</div>
+          </div>
+          <div className =  "text-white text-sm card">Net Balance: 
+            <div className = "text-white text-2xl">${summary["net balance"]}</div>
+          </div>
+        </div> 
+        <div className = "card col-span-2 row-span-2">Balance Overview</div>
+      </div>
+
+    </div>
   )
 }
 
