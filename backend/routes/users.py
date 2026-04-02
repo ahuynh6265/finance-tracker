22081ac57a8c4ca03d5b58ad584c3309fb75f844
Alex Huynh <174913689+ahuynh6265@users.noreply.github.com>
2026-04-02 15:50:22 -0400

    removed GET And POST routes from users, updated PUT and DELETE routes with current id token

--- backend/routes/users.py
+++ backend/routes/users.py
@@ -3,36 +3,24 @@ from sqlalchemy.orm import Session
 from database import get_db 
 from models import User 
 from schemas import UserCreate, UserResponse
-from dependencies import user_lookup
+import auth 
 
 router = APIRouter() 
 
-@router.get("/users/{user_id}", response_model=UserResponse)
-def get_user(user_id: int, db: Session = Depends(get_db)):
-  user = user_lookup(user_id, db)
-  return user 
-
-@router.post("/users", response_model=list[UserResponse], status_code=status.HTTP_201_CREATED)
-def create_user(users_data: list[UserCreate], db: Session = Depends(get_db)):
-  new_users = [User (name = u.name, email = u.email) for u in users_data]
-  db.add_all(new_users)
-  db.commit() 
-  for user in new_users:
-    db.refresh(user)
-  return new_users
-
-@router.put("/users/{user_id}", response_model=UserResponse)
-def update_user(user_id: int, user_data: UserCreate, db: Session = Depends(get_db)):
-  user = user_lookup(user_id, db)
+@router.put("/users/me", response_model=UserResponse)
+def update_user(user_data: UserCreate, db: Session = Depends(get_db), current_user: dict = Depends(auth.get_current_user)):
+  user = db.query(User).filter(User.id == current_user["id"]).first()
   user.name = user_data.name
   user.email = user_data.email 
+  
   db.commit() 
   db.refresh(user)
   return user
 
-@router.delete("/users/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_user(user_id: int, db: Session = Depends(get_db)):
-  user = user_lookup(user_id, db)
+@router.delete("/users/me", status_code=status.HTTP_204_NO_CONTENT)
+def delete_user(db: Session = Depends(get_db), current_user: dict = Depends(auth.get_current_user)):
+  user = db.query(User).filter(User.id == current_user["id"]).first()
+
   db.delete(user)
   db.commit()
   return 
