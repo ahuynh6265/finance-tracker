7bc631eda59caf88a0ed9aa508c6a25262855210
Alex Huynh <174913689+ahuynh6265@users.noreply.github.com>
2026-04-05 21:53:10 -0400

    updated post routes, no lists

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -6,17 +6,17 @@ const api = axios.create({baseURL: BASE_URL})
 export const getSummary = () => api.get(`/summary`)
 
 export const getAccounts = () => api.get(`/accounts`)
-export const createAccount = (account_data) => api.post(`/accounts`, [account_data])
+export const createAccount = (account_data) => api.post(`/accounts`, account_data)
 export const deleteAccount = (account_id) => api.delete(`/accounts/${account_id}`)
 export const updateAccount = (account_id, account_data) => api.put(`/accounts/${account_id}`, account_data)
 
 export const getCategories = () => api.get(`/categories`)
-export const createCategory = (category_data) => api.post(`/categories`, [category_data])
+export const createCategory = (category_data) => api.post(`/categories`, category_data)
 export const deleteCategory = (category_id) => api.delete(`/categories/${category_id}`)
 export const updateCategory = (category_id, category_data) => api.put(`/categories/${category_id}`, category_data)
 
 export const getTransactions = () => api.get(`/transactions`)
-export const createTransaction = (transaction_data) => api.post(`/transactions`, [transaction_data])
+export const createTransaction = (transaction_data) => api.post(`/transactions`, transaction_data)
 export const deleteTransaction = (transaction_id) => api.delete(`/transactions/${transaction_id}`)
 export const updateTransaction = (transaction_id, transaction_data) => api.put(`/transactions/${transaction_id}`, transaction_data)
 
