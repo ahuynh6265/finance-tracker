75e260b99bb3b5395138eaf554cc302eeb14a523
Alex Huynh <174913689+ahuynh6265@users.noreply.github.com>
2026-04-09 22:38:02 -0400

    transfer logic added, withdrawal route and logic created

--- backend/routes/goals.py
+++ backend/routes/goals.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, HTTPException, Depends, status
 from sqlalchemy.orm import Session 
 from database import get_db 
-from models import User, Goal 
+from models import User, Goal, Transaction, Category
 from schemas import GoalCreate, GoalResponse, GoalUpdate
 from dependencies import goal_lookup, account_lookup
 import auth 
+from datetime import date
 
 router =  APIRouter()
 
@@ -55,13 +56,58 @@ def update_goal(goal_id: int, goal_data: GoalCreate, db: Session = Depends(get_d
 def update_current_amount(goal_id, update_data: GoalUpdate, db: Session = Depends(get_db), current_user: dict = Depends(auth.get_current_user)):
   goal = goal_lookup(goal_id, db, current_user["id"])
   account = account_lookup(update_data.account_id, db, current_user["id"])
+  category = db.query(Category).filter(Category.user_id == current_user["id"], Category.name == "Transfer").first()
 
   if update_data.amount > account.balance:
     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Account does not have enough money for this transfer.")
+  
+  create_transaction = Transaction(
+    user_id = current_user["id"], 
+    account_id = update_data.account_id, 
+    category_id = category.id, 
+    destination_goal_id = goal.id, 
+    amount = update_data.amount, 
+    transaction_type = "transfer", 
+    description  = f"Transfer to {goal.name}",
+    date = date.today()
+  )
 
   account.balance -= update_data.amount  
   goal.current_amount += update_data.amount
+  db.add(create_transaction)
   db.commit()
+  db.refresh(create_transaction)
+  db.refresh(account)
+  db.refresh(goal)
+  return goal
+
+@router.put("/goals/{goal_id}/withdraw", response_model=GoalResponse)
+def withdraw_from_goal(goal_id: int, update_data: GoalUpdate, db: Session = Depends(get_db), current_user: dict = Depends(auth.get_current_user)):
+  goal = goal_lookup(goal_id, db, current_user["id"])
+  account = account_lookup(update_data.account_id, db, current_user["id"])
+  category = db.query(Category).filter(Category.user_id == current_user["id"], Category.name == "Transfer").first()
+
+  if update_data.amount > goal.current_amount:
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Can not withdraw more than you currently have in the fund.")
+  
+  #SOURCE AND DESTINATION SWITCHED BECAUSE USER IS WITHDRAWING FROM GOAL FUND MEANING GOAL ID IS NOW THE SOURCE AND THE ACCOUNT THE MONEY IS BEING RETURNED TO IS THE SOURCE
+  #note that for withdrawals account id does not have to be the same account id used to fund goal, can be withdrawn into another of the user's account
+  create_transaction = Transaction(
+    user_id = current_user["id"], 
+    account_id = update_data.account_id, #DESTINATION
+    category_id = category.id, 
+    destination_goal_id = goal.id, #SOURCE
+    amount = update_data.amount, 
+    transaction_type = "transfer", 
+    description  = f"Withdrawal from {goal.name}",
+    date = date.today()
+  )
+  #logic is flipped as well
+  goal.current_amount -= update_data.amount 
+  account.balance += update_data.amount 
+  db.add(create_transaction)
+  db.commit()
+  db.refresh(create_transaction)
   db.refresh(account)
   db.refresh(goal)
   return goal
