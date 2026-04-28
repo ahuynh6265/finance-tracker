c25e6ec9261c0c9f383f0b3ac873efc96517e761
Alex Huynh <174913689+ahuynh6265@users.noreply.github.com>
2026-04-28 18:54:20 -0400

    daily beat schedule for process_due_subscriptions

--- backend/celery_app.py
+++ backend/celery_app.py
@@ -1,14 +1,22 @@
 from dotenv import load_dotenv
 import os 
 from celery import Celery 
+from celery.schedules import crontab
 
 load_dotenv()
 REDIS_URL = os.getenv("REDIS_URL")
 
 celery_app = Celery(__name__)
+
 celery_app.conf.update(
   broker_url = REDIS_URL, 
   timezone = "UTC",
   broker_connection_retry_on_startup = True,
-  imports = ("tasks",)
+  imports = ("tasks",),
+  beat_schedule = {
+  "daily-due-subscriptions" : {
+    "task": "tasks.process_due_subscriptions",
+    "schedule": crontab(hour=0, minute=0)
+    },
+  }
 )
